050ebd0ecf4cbd62c52f30de98ea2a9029bb1668
yero0214 <yero0214@gmail.com>
2021-06-14 17:09:59 +0900

    commit

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,19 +2,30 @@
 <div class="container">
     <h1>Login</h1>
     <span>id</span>
-    <input type="text"><br>
+    <input type="text" v-model="id"><br>
     <span>pwd</span>
-    <input type="password"><br>
+    <input type="password" v-model="pwd"><br>
     <div @click="login()">Login</div>
     <router-link to="/Register">Register</router-link>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data(){
+    return{
+      id:'',
+      pwd:'',
+    }
+  },
   methods:{
     login(){
-      this.$router.push("/Home")
+      axios.post("http://localhost:8010/api/login",{"id":"test","pwd":"1234"})
+      .then(result => console.log(result.data))
+      .catch(error => console.log(error))
+      // this.$router.push("/Home")
     }
   }
 }
